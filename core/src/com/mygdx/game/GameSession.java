bb820121c50fba8246a41f4f4c1e8e0b17db2437
KasarLooper <kasar.looper@gmail.com>
2024-06-14 07:23:31 +0300

    Add trash

---
+++ core/src/com/mygdx/game/GameSession.java
@@ -0,0 +1,25 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.utils.TimeUtils;
+
+public class GameSession {
+    long sessionStartTime;
+    long nextTrashSpawnTime;
+
+    public void startGame() {
+        sessionStartTime = TimeUtils.millis();
+        nextTrashSpawnTime = (long) (sessionStartTime + GameSettings.STARTING_TRASH_APPEARANCE_COOL_DOWN * getTrashPeriodCoolDown());
+    }
+
+    public boolean shouldSpawnTrash() {
+        if (TimeUtils.millis() >= nextTrashSpawnTime) {
+            nextTrashSpawnTime += (long) (GameSettings.STARTING_TRASH_APPEARANCE_COOL_DOWN * getTrashPeriodCoolDown());
+            return true;
+        }
+        return false;
+    }
+
+    private float getTrashPeriodCoolDown() {
+        return (float) Math.exp(-0.001 * (TimeUtils.millis() - sessionStartTime + 1) / 1000);
+    }
+}
---
+++ core/src/com/mygdx/game/objects/TrashObject.java
@@ -0,0 +1,31 @@
+package com.mygdx.game.objects;
+
+import static com.mygdx.game.GameSettings.SCALE;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import com.mygdx.game.GameSettings;
+
+import java.util.Random;
+
+public class TrashObject extends GameObject {
+    static Random random = new Random();
+    static int paddingHorizontal = 30;
+    public TrashObject(int width, int height, String texturePath, World world) {
+        super(width / 2 + paddingHorizontal + random.nextInt(GameSettings.SCREEN_WIDTH - 2 * paddingHorizontal - width),
+                GameSettings.SCREEN_HEIGHT + height / 2, width, height, texturePath, world);
+        body.setLinearVelocity(new Vector2(0, -GameSettings.TRASH_VELOCITY));
+    }
+
+    @Override
+    public void draw(Batch batch) {
+        super.draw(batch);
+        //System.out.printf("Position: (%d; %d)\n", getX(), getY());
+        //System.out.printf("Speed: (%f; %f)\n", (body.getLinearVelocity().x / SCALE), (body.getLinearVelocity().y / SCALE));
+    }
+
+    public boolean isInFrame() {
+        return getY() > -height / 2;
+    }
+}
